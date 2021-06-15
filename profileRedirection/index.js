e64e2f1d4925dc9d38f3ce1ea7d86db0e12ef62d
ARC Team <dev@producthackers.com>
2021-06-15 17:39:07 +0200

    fix: fixed lang redirection problem and changed assets structure

--- profileRedirection/index.js
+++ profileRedirection/index.js
@@ -1,30 +1,29 @@
 function handler(event) {
-    console.log(event.request.headers);
     var request = event.request;
     var headers = request.headers;
-    if(request.uri !=="/notLogged.html" && (!headers.authorization || (headers.authorization && headers.authorization.value.indexOf('Bearer XX') < 0))){
-        var response = {
-          statusCode: 302,
-          statusDescription: 'Found',
-          headers: {
-            "location": { "value": "/notLogged.html" }
-          }
+    if(request.uri !=="/notLogged.html"){
+        if(!headers.authorization || (headers.authorization && headers.authorization.value.indexOf('Bearer XX') < 0)){
+            var response = {
+              statusCode: 302,
+              statusDescription: 'Found',
+              headers: {
+                "location": { "value": "/notLogged.html" }
+              }
+            }
+            return response;
         }
-        return response;
-    }
-    if (headers['cloudfront-viewer-country']) {
-      var countryCode = headers['cloudfront-viewer-country'].value.toLowerCase();
-      if (["fr","en"].includes(countryCode)) {
-        var response = {
-          statusCode: 302,
-          statusDescription: 'Found',
-          headers: {
-            "location": { "value": '/profile_' + countryCode + '.html' }
-          }
+        if (request.uri.substr(3,1) != '/' && headers['cloudfront-viewer-country']) {
+          var countryCode = headers['cloudfront-viewer-country'].value.toLowerCase();
+          var response = {
+              statusCode: 302,
+              statusDescription: 'Found',
+              headers: {
+                "location": { "value": '/' + (["fr","es"].includes(countryCode) ? countryCode : "en") + request.uri }
+              }
+            }
+          return response;
         }
-        return response;
-      }
     }
-    
+
     return request;
 }
